81cea677a9b609e4401a5af173cd412a7717843b
raf-mat <raphael.matthey.mail@gmail.com>
2025-01-12 22:45:42 +0100

    update classes2.py and added exceptions.py

--- web_python/PYTHON/classes2.py
+++ web_python/PYTHON/classes2.py
@@ -1,6 +1,6 @@
 # define a class called Flight with one argument : capacity
 class Flight:
-
+    # Method to create new flight with given capacity
     def __init__(self, capacity):
          #we store the value into a variable called capacity    
         self.capacity = capacity
@@ -9,20 +9,21 @@ class Flight:
 
  # let's define a new method or function to add a new passenger, Because this is a method that's going to work on an individual object, 
     # we need some way of referencing that object itself. So we'll use the keyword "self" again. 
-
+    # Method to add a passenger to the flight:
     def add_passenger(self, name):
         if not self.open_seats():
             return False
         self.passengers.append(name)
         return True
-
+    # Method to return number of open seats
     def open_seats(self):
         return self.capacity - len(self.passengers)
 
 #create a flight with a capacity of three with a list of empty passenger
 flight = Flight(capacity=3)
-
+# Create a list of people
 people = ["Harry", "Ron", "Hermione", "Ginny"]
+# Attempt to add each person in the list to a flight
 for person in people:
     if flight.add_passenger(person):
         print(f"Added {person} to flight.")
