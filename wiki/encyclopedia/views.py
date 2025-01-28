8b5780498a7a4bbaeae383cbf2e92388f0e2cc3f
raf-mat <raphael.matthey.mail@gmail.com>
2025-01-28 23:25:24 +0100

    search bar working

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -17,17 +17,6 @@ def convert_md_to_html(title):
         return markdowner.convert(content)
      
 def index(request):
-    if request.method == "POST":
-
-        form = Search(request.POST)
-        if query.is_valid():
-            q = form.cleaned_data["q"]
-
-            return HttpResponseRedirect(reverse("encyclopedia/index.html"))
-        
-        else:
-            return render(request, "encyclopedia/error.html")
-        
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),
         "form": Search()
@@ -43,3 +32,25 @@ def entry(request, title):
         return render(request, "encyclopedia/entry.html", {
             "content": html_content
         })
+
+def search(request):
+    if request.method =="POST":
+        # storing the value user input into entry_search variable
+        entry_search = request.POST['q']
+        # converting entry search md file into html thanks to import markdown functions + storing into html_content variable
+        html_content = convert_md_to_html(entry_search)
+        # if html_content is True 
+        if html_content is not None:
+            return render(request, "encyclopedia/entry.html", {
+                "title": entry_search,
+                "content": html_content
+            })
+        else:
+            allEntries = util.list_entries()
+            recommendation = []
+            for entry in allEntries:
+                if entry_search.lower() in entry.lower():
+                    recommendation.append(entry)
+            return render(request, "encyclopedia/search.html",{
+                "recommendation": recommendation
+                })
