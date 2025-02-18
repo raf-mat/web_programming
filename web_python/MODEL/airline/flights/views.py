c32e7cd50eab019bee5a92c124e2da9cca842708
raf-mat <raphael.matthey.mail@gmail.com>
2025-02-18 22:57:22 +0100

    added a new path to see the details of a flight /<int:flight_id> and i have added a passenger model in order to book them on a flight with django admin and display the passenger booked on a flight thanks to the flight template

--- web_python/MODEL/airline/flights/views.py
+++ web_python/MODEL/airline/flights/views.py
@@ -7,3 +7,13 @@ def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
     })
+
+#
+def flight(request, flight_id):
+     #get me the flight whose id or pk (primary key)= flight_id
+    flight = Flight.objects.get(pk=flight_id)
+    return render (request, "flights/flight.html", {
+        "flight":flight,
+        #passengers is the related_name of flights variable into Passenger model
+        "passengers": flight.passengers.all()
+    })
